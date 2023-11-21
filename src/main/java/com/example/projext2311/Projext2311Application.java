aedac1ba566e1cdb09e9b0a057e9dcfdc3b9e55a
kimjh <scottgg12345@gmail.com>
2023-11-21 16:40:14 +0900

    chapter1_oH

---
+++ src/main/java/com/example/projext2311/Projext2311Application.java
@@ -0,0 +1,13 @@
+package com.example.projext2311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projext2311Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projext2311Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projext2311/Projext2311ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projext2311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projext2311ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
